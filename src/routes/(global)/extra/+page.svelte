82a1c463b0a792c9448a2f105255efd5fca4c807
Zodiac <arieschaf@gmail.com>
2025-07-23 19:18:23 -0400

    adding to etc page

--- src/routes/(global)/extra/+page.svelte
+++ src/routes/(global)/extra/+page.svelte
@@ -2,6 +2,14 @@
 
 <div class="mainBody">
 
+    <div class="infoBox">
+        <h3>So how do zodiacs for alters work?</h3>
+
+        <p>To be honest, it's just whatever makes sense for the individual. There's no real science involved in what we do for ourselves, it's all for fun! Take it as seriously or unseriously as you feel like.</p>
+        <p>(Do be respectful, however)</p>
+        <p>For some of us, we get our "main three" signs by doing a chart based on what time an individual was logged in PK. For some others, the signs are based on source. For yet more, the sign is just vibes.</p>
+    </div>
+
     
 
 </div>
@@ -16,5 +24,6 @@
         border-radius: 10px;
         box-shadow: 5px 5px #00000090;
     }
+
 
 </style>
