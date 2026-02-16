36d39f97de2aeaba4c04f69b23db17a5d227b5c5
Zodiac <arieschaf@gmail.com>
2026-02-15 20:19:04 -0500

    fronter tool fixed tysm iris

--- src/routes/front/+page.svelte
+++ src/routes/front/+page.svelte
@@ -1,49 +1,73 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+    import pkfronters from "irys-webby/dist/iryscc/fronters/pkfronters.js";
+    onMount(() => {
+        pkfronters.renderAll();
+    });
+</script>
+
 <div data-pkfronters-system="zodiac" data-pkfronters-type="card" data-pkfronters-options="dn"></div>
-<script src="https://irys.cc/fronters/pkfronters.js" async></script>
 
 <style>
+:global {
+    /* this is the default pkfronters.css */
+    .pkfronters--member-list--pronouns,
+    .pkfronters--member-card--pronouns
+    {
+        word-break: break-word;
+    }
 
-.pkfronters--member-list--pronouns, .pkfronters--member-card--pronouns {
-    word-break: break-word;
-}
-.pkfronters--member-card--pronouns {
-    text-align: center;
-}
-.pkfronters--member-list--pronouns::before, .pkfronters--member-card--pronouns::before {
-    content:" (";
-}
-.pkfronters--member-list--pronouns::after, .pkfronters--member-card--pronouns::after {
-    content:") ";
-}
-.pkfronters--member-card-container {
-    display: flex;
-    flex-flow: row wrap;
-    gap: 1rem;
-}
-.pkfronters--member-card {
-    display:flex;
-    flex-flow:column wrap;
-    align-items:center;
-    max-width:8em;
-    background:#fff;
-    color:#000;
-    padding:1rem;
-    border-radius:4px;
-    border:4px solid rgba(0,0,0,0);
-}
-.pkfronters--member-card--avatar {
-    min-width:6rem;
-    width:6rem;
-    min-height:6rem;
-    height:6rem;
-    margin:0 0 .5rem 0;
-    border-radius:4px;
-    border:4px solid rgba(0,0,0,0);
-}
-.pkfronters--member-card--name {
-    text-align:center;
-} 
-/*# sourceMappingURL=pkfronters.css.map */
+    .pkfronters--member-card--pronouns
+    {
+        text-align: center;
+    }
 
+    .pkfronters--member-list--pronouns::before,
+    .pkfronters--member-card--pronouns::before
+    {
+        content: '\0020(';
+    }
 
+    .pkfronters--member-list--pronouns::after,
+    .pkfronters--member-card--pronouns::after
+    {
+        content: ')\0020';
+    }
+
+    .pkfronters--member-card-container
+    {
+        display: flex;
+        flex-flow: row wrap;
+        gap: 1rem;
+    }
+
+    .pkfronters--member-card
+    {
+        display: flex;
+        flex-flow: column wrap;
+        align-items: center;
+        max-width: 8em;
+        background: #fff;
+        color: #000;
+        padding: 1rem;
+        border-radius: 4px;
+        border: 4px solid transparent;
+    }
+
+    .pkfronters--member-card--avatar
+    {
+        min-width: 6rem;
+        width: 6rem;
+        min-height: 6rem;
+        height: 6rem;
+        margin: 0 0 0.5rem 0;
+        border-radius: 4px;
+        border: 4px solid transparent;
+    }
+
+    .pkfronters--member-card--name
+    {
+        text-align: center;
+    }
+}
 </style>
