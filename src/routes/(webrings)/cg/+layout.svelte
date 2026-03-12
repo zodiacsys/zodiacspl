9ec1f682ec5e576ded862096a023a6e35f5b4cc6
Zodiac <arieschaf@gmail.com>
2026-03-12 09:27:04 -0400

    added styling to the index page of the webring

---
+++ src/routes/(webrings)/cg/+layout.svelte
@@ -0,0 +1,58 @@
+<div class="pageContent">
+
+    <div class="headerBox">
+        <h1 class="titleName">The Color Gang Webring!</h1>
+    </div>
+
+    <div class="mainContent">
+        <slot />
+    </div>
+
+</div>
+
+
+
+<style>
+
+    :global(body) {
+        background-color: #eae4d4;
+        color: #292723;
+        font-family: "Atkinson Hyperlegible", sans-serif;
+        font-size: 1.25em;
+    }
+
+    h1, h2, h3, p {
+        margin: 0;
+    }
+
+    .pageContent {
+        width: 65vw;
+        margin: auto;
+    }
+
+    .titleName {
+        margin-left: 1.5vw;
+    }
+
+    .headerBox {
+        background-color: #d0c8b4;
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        height: 8vh;
+        border-radius: 15px;
+        margin-top: 2vh;
+    }
+
+    .mainContent {
+        background-color: #d0c8b4;
+        display: flex;
+        min-height: 82vh;
+        max-height: max-content;
+        border-radius: 15px;
+        margin-top: 2vh;
+        padding-left: 1.5em;
+        padding-right: 1.5em;
+    }
+
+</style>
